73b1156ce83fcf91466a975bfc6eaf20f6bed111
Kyle Butler <theokyle@gmail.com>
2025-12-14 08:12:19 -0600

    add guess-the-number game

---
+++ guess-the-number/src/app/game/game.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-game',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './game.html',
+  styleUrl: './game.scss',
+})
+export class Game {
+    secretNumber = this.generateRandomNumber();
+    attemptsLeft = 10;
+    guessedNumber?: number;
+    feedbackMessage = '';
+    gameOver = false;
+
+    private static readonly MAX_NUMBER = 100;
+    private static readonly MAX_ATTEMPTS = 10;
+
+    private generateRandomNumber() {
+      return Math.floor(Math.random() * Game.MAX_NUMBER + 1)
+    }
+
+    public isValidGuess(guess?: number): boolean {
+      return (guess !== undefined && guess >= 1 && guess <= Game.MAX_NUMBER)
+    }
+
+    submitGuess(): void {
+      if(!this.isValidGuess(this.guessedNumber)) {
+        this.feedbackMessage = `Enter a number between 1 and ${Game.MAX_NUMBER}.`;
+        return;
+      }
+      this.attemptsLeft--;
+      this.evaluateGuess();
+    }
+
+    private evaluateGuess(): void {
+      if(this.guessedNumber === this.secretNumber) {
+        this.endGame(true);
+      } else if (this.attemptsLeft === 0) {
+        this.endGame(false);
+      } else {
+        this.feedbackMessage = this.guessedNumber! > this.secretNumber 
+          ? 'Too High! Try again.'
+          : 'Too Low! Try again.'
+      }
+    }
+
+    private endGame(isWin:boolean): void {
+      this.gameOver = true;
+      this.feedbackMessage = isWin? 'Congratulations! You guessed the correct number!' : `Game over! The correct number was ${this.secretNumber}`
+    }
+
+    resetGame(): void {
+      this.secretNumber = this.generateRandomNumber();
+      this.attemptsLeft = Game.MAX_ATTEMPTS;
+      this.guessedNumber = undefined;
+      this.feedbackMessage = '';
+      this.gameOver = false;
+    }
+}
